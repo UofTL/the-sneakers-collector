7afebe1f1f71a3f2f4834cadb8d631e3def0d1b8
Eleonore <eleonore.caclard@gmail.com>
2021-09-21 20:16:17 -0400

    all publich Js file done by Eleonore

---
+++ public/javascript/delete-sneakers.js
@@ -0,0 +1,26 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/sneakers/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            sneakers_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/inventory/');
+    } else {
+        alert(response.statusText);
+    }
+
+}
+
+document.querySelector('#delete-sneakers-btn').addEventListener('click', deleteFormHandler);
---
+++ public/javascript/sneakers.js
@@ -0,0 +1,31 @@
+async function newSneakersHandler(event) {
+    event.preventDefault();
+
+    const name = document.querySelector('#sneakers').value.trim();
+    const sneakers_size = document.querySelector('#size').value.trim();
+    const price_paid = document.querySelector('#retail-price').value.trim();
+    const resell_value = document.querySelector('#resale-price').value.trim();
+    const notes = document.querySelector('#notes').value.trim();
+
+    const response = await fetch(`/api/sneakers`, {
+        method: 'POST',
+        body: JSON.stringify({
+            name,
+            sneakers_size,
+            price_paid,
+            resell_value,
+            notes
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/inventory');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-sneakers').addEventListener('submit', newSneakersHandler);
