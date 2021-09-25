2ea577be74dea7c26c3ae6dfc40fabb1272bcab3
Eleonore <eleonore.caclard@gmail.com>
2021-09-25 11:20:21 -0400

    squilize error fixed

--- models/Vote.js
+++ models/Vote.js
@@ -3,37 +3,34 @@ const sequelize = require('../config/connection');
 
 class Vote extends Model {}
 
-Vote.init(
-    {
-      id: {
+Vote.init({
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      },
-      whiskey_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'post',
-          key: 'id'
-        }
-      }
     },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'vote'
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    sneakers_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'sneakers',
+            key: 'id'
+        }
     }
-  );
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vote'
+});
 
 module.exports = Vote;
